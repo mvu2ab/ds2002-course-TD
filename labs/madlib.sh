9d2a6f6ffa7e5975cad3576671677515ff7ad3e7
bob <143120019+mvu2ab@users.noreply.github.com>
2024-02-21 22:30:36 -0500

    Lab 3

---
+++ labs/madlib.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib"
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me a noun: " N2
+read -p "3. Please give me a verb: " V3
+read -p "4. Please give me an adverb: " ADV4
+read -p "5. Please give me an adjective: " ADJ5
+read -p "6. Please give me a noun: " N6
+read -p "7. Please give me a verb: " V7
+read -p "8. Please give me an adverb: " ADV8
+
+echo "Once upon a time there was a $ADJ1 princess. She loved her $N2. Together they $V3. They did it $ADV4. Then one day the $ADJ5 prince came along. He had a $N6. The four of them $V7 $ADV8."
